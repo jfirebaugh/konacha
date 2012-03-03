d7bfec480d8752af2d1a6ae6482761ae1d3f1b7c
Alexander Kern <alex@kernul.com>
2012-03-02 18:41:24 -0800

    Fix bug where files were accidentally processed.

--- app/helpers/konacha/specs_helper.rb
+++ app/helpers/konacha/specs_helper.rb
@@ -3,10 +3,10 @@ module Konacha
     def spec_include_tag(*specs)
       bundler = SpecBundler.new(asset_paths)
       bundler.add_specs(specs)
-      assets = bundler.assets
+      assets = bundler.assets.map { |a| asset_path(a) }
 
-      asset_paths = assets.map { |a| asset_path(a) }
-      javascript_include_tag *asset_paths, :debug => false
+      @javascript_include ||= ActionView::Helpers::AssetTagHelper::JavascriptIncludeTag.new(config, asset_paths)
+      @javascript_include.include_tag(*assets)
     end
   end
 end
