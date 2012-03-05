c3629a629e271615be91c2c745f8c61248eabbc6
Jo Liss <joliss42@gmail.com>
2012-03-05 11:04:09 +0100

    Preload gems to avoid tilt warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rspec/rails"
 require "rspec/autorun"
 
+# Preload to avoid occasional tilt warnings about thread safety
+require "coffee_script"
+require "ejs"
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
