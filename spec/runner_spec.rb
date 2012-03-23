b78187afd7143ae2f24049e9d318bd8931e109de
Jo Liss <joliss42@gmail.com>
2012-03-23 22:40:30 +0100

    Tighten up #run spec

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -15,14 +15,12 @@ describe Konacha::Runner do
     it "prints results to the output" do
       buffer.rewind
       results = buffer.read
-      # "should include" gives us nice multi-line error messages if there is
-      # more than one failure
-      results.should include 'examples, 1 failure'
       # Failure output present?
       results.should include 'F'
       results.should include 'expected 4 to equal 5'
-      # Enough examples run?
-      results.should match /[1-9][0-9]+ examples, 1 failure/
+      # Summary and dots
+      results.should include "#{runner.examples.length} examples, 1 failure"
+      results.should match /^[.F]{#{runner.examples.length}}$/
     end
   end
 end
