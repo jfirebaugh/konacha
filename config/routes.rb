f8cb30b2d0403f199960ff3c6c56256aae155199
John Firebaugh <john.firebaugh@gmail.com>
2013-03-04 08:20:17 -0800

    Use `root`

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Konacha::Engine.routes.draw do
   get '/iframe/*name' => 'specs#iframe', :format => false, :as => :iframe
-  get '/'             => 'specs#parent'
-  get '*path'         => 'specs#parent'
+  root :to    => 'specs#parent'
+  get '*path' => 'specs#parent'
 end
