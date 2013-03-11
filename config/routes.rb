15b31e7962ff1067a26f6f6941baa8092c166ecd
John Firebaugh <john.firebaugh@gmail.com>
2013-03-10 21:18:18 -0700

    Use `:format => false` for the other catchall route
    This fixes a Rails 4 compatibility issue. I filed an
    upstream bug for investigation:
    
    https://github.com/rails/rails/issues/9654

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Konacha::Engine.routes.draw do
   get '/iframe/*name' => 'specs#iframe', :format => false, :as => :iframe
   root :to    => 'specs#parent'
-  get '*path' => 'specs#parent'
+  get '*path' => 'specs#parent', :format => false
 end
