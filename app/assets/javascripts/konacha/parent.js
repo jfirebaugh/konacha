54563a3a8420275cf790377b8f5fa79720dec185
Geoff Bucks <gsbucks@gmail.com>
2014-03-12 21:04:17 -0700

    Override mocha links to utilize the konacha path parameter.
    Thus only the particular iframes for the mocha suite/test that
    was clicked will be rendered.

--- app/assets/javascripts/konacha/parent.js
+++ app/assets/javascripts/konacha/parent.js
@@ -4,6 +4,23 @@ window.onload = function () {
     return a.path.localeCompare(b.path);
   });
 
+  var originalTestUrl = Mocha.reporters.HTML.prototype.testURL;
+
+  Mocha.reporters.HTML.prototype.suiteURL = function(suite) {
+    var specFilePath,
+        grepString = originalTestUrl(suite),
+        currentSuite = suite;
+
+    while (!(specFilePath = currentSuite.path)) {
+      currentSuite = currentSuite.parent
+    }
+
+    specFilePath = /([^\.]*)/.exec(specFilePath)[1];
+    return location.protocol+'//'+location.host+'/'+specFilePath+grepString;
+  };
+
+  Mocha.reporters.HTML.prototype.testURL = Mocha.reporters.HTML.prototype.suiteURL;
+
   // Check that all iframes loaded successfully.
   var iframes = document.getElementsByTagName('iframe');
   for (var i = 0; i < iframes.length; ++i) {
