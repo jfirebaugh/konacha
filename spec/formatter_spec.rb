6448dc5f7a1ca247581ffad6a38a8200f14128b6
Matthijs Groen <matthijs.groen@gmail.com>
2014-07-24 14:58:53 +0200

    Revert "Update spec style"
    This reverts commit fcf302dacecc597cd6e09f1515c5ae8b64843e5a.

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -4,19 +4,19 @@ describe Konacha::Formatter do
   let(:io) { StringIO.new }
   subject { described_class.new(io) }
 
-  shared_examples 'test result' do |method, dot|
-    it 'stores the example in the examples array' do
+  shared_examples "test result" do |method, dot|
+    it "stores the example in the examples array" do
       subject.send(method, nil)
-      expect(subject.examples).to be_present
+      subject.examples.should be_present
     end
 
-    it 'outputs the dot' do
+    it "outputs the dot" do
       subject.send(method, nil)
       io.rewind
-      expect(io.read).to eql(dot)
+      io.read.should eql(dot)
     end
 
-    it 'uses colors when tty' do
+    it "uses colors when tty" do
       allow(io).to receive(:tty?).and_return(true)
       subject.send(method, nil)
       io.rewind
@@ -24,53 +24,53 @@ describe Konacha::Formatter do
     end
   end
 
-  describe '#example_passed' do
-    it_behaves_like 'test result', :example_passed, '.'
+  describe "#example_passed" do
+    it_behaves_like "test result", :example_passed, "."
   end
 
-  describe '#example_failed' do
-    it_behaves_like 'test result', :example_failed, 'F'
+  describe "#example_failed" do
+    it_behaves_like "test result", :example_failed, "F"
   end
 
-  describe '#example_pending' do
-    it_behaves_like 'test result', :example_pending, 'P'
+  describe "#example_pending" do
+    it_behaves_like "test result", :example_pending, "P"
   end
 
-  describe '#dump_pending' do
-    let(:example) { double('example', :pending? => true, :full_description => 'Pending example') }
+  describe "#dump_pending" do
+    let(:example) { double('example', :pending? => true, :full_description => "Pending example") }
     before { subject.stub(:examples => [example]) }
 
-    it 'outputs the pending message' do
+    it "outputs the pending message" do
       subject.dump_pending
       io.rewind
-      expect(io.read).to include('  Pending: Pending example')
+      io.read.should include("  Pending: Pending example")
     end
   end
 
-  describe '#dump_failures' do
+  describe "#dump_failures" do
     let(:example) do
       double('example',
              :failed?            => true,
-             :full_description   => 'Failed example',
+             :full_description   => "Failed example",
              :exception          => double('exception',
-                                           :message   => 'exception',
+                                           :message   => "exception",
                                            :backtrace => nil))
     end
 
     before { subject.stub(:examples => [example]) }
 
-    it 'outputs the failure message' do
+    it "outputs the failure message" do
       subject.dump_failures
       io.rewind
-      expect(io.read).to include("  Failed: Failed example\n    exception")
+      io.read.should include("  Failed: Failed example\n    exception")
     end
   end
 
-  describe '#dump_summary' do
-    it 'outputs the summary' do
+  describe "#dump_summary" do
+    it "outputs the summary" do
       subject.dump_summary(10, 10, 2, 3)
       io.rewind
-      expect(io.read).to eql "\nFinished in 10.00 seconds\n10 examples, 2 failed, 3 pending\n"
+      io.read.should == "\nFinished in 10.00 seconds\n10 examples, 2 failed, 3 pending\n"
     end
   end
 end
